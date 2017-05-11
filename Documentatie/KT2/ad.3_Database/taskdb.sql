0ede9c371390ac913ba09517e8d394f795b5dbf3
Patrick <zammypatrick@gmail.com>
2017-05-11 11:32:14 +0200

    exported db

---
+++ Documentatie/KT2/ad.3_Database/taskdb.sql
@@ -0,0 +1,79 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.23-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for taskdb
+CREATE DATABASE IF NOT EXISTS `taskdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `taskdb`;
+
+-- Dumping structure for table taskdb.appointment
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `Id` int(10) unsigned NOT NULL,
+  `UserId` int(10) unsigned NOT NULL,
+  `Description` varchar(64) NOT NULL,
+  `Date` date NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `UserId` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table taskdb.grade
+CREATE TABLE IF NOT EXISTS `grade` (
+  `RowIndex` int(10) unsigned NOT NULL,
+  `UserId` int(10) unsigned NOT NULL,
+  `ColumnIndex` int(10) unsigned NOT NULL,
+  `Grade` float unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`RowIndex`),
+  KEY `UserId` (`UserId`),
+  CONSTRAINT `SubjectFK` FOREIGN KEY (`RowIndex`) REFERENCES `subject` (`RowIndex`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table taskdb.subject
+CREATE TABLE IF NOT EXISTS `subject` (
+  `RowIndex` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `UserId` int(10) unsigned NOT NULL,
+  `Name` varchar(64) NOT NULL,
+  PRIMARY KEY (`RowIndex`),
+  KEY `UserId` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table taskdb.task
+CREATE TABLE IF NOT EXISTS `task` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `UserId` int(10) unsigned NOT NULL,
+  `TaskTitle` varchar(64) NOT NULL,
+  `TaskDate` date NOT NULL,
+  `TaskDuration` time NOT NULL,
+  `TaskRepeats` tinyint(3) unsigned NOT NULL,
+  `TaskLabel` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `UserFK` (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table taskdb.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Username` varchar(64) NOT NULL,
+  `Password` varchar(64) NOT NULL,
+  `Role` varchar(64) NOT NULL DEFAULT 'Member',
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Username` (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
