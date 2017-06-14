d589177522574ee287972681f4907c785ad155aa
Steven189 <Steven.logghe@hotmail.com>
2017-06-14 11:07:27 +0200

    Acceptatie test en designer update

--- Documentatie/KT2/ad.8_Solution/SamenSterkOffline/Views/EditAppointment.Designer.cs
+++ Documentatie/KT2/ad.8_Solution/SamenSterkOffline/Views/EditAppointment.Designer.cs
@@ -108,6 +108,7 @@
             this.MinimizeBox = false;
             this.Name = "EditAppointment";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Wijzig datum";
             this.ResumeLayout(false);
             this.PerformLayout();
